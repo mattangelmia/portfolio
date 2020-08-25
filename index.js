ae729267b2df30485c896ee848c75becc2ff79b6
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-08-25 18:48:45 -0400

    contactme

--- index.js
+++ index.js
@@ -8,3 +8,22 @@ function hover(){
 }
 
 portfolioItems.addEventListener('mouseover',hover)
+
+
+document.querySelector("form").onsubmit = function (e){
+    e.preventDefault()
+    let comment = document.querySelector("#comments input").value
+    
+    console.log(comment)
+    axios.post("http://ironrest.herokuapp.com/angelcomments",{comment:comment})
+    .then(function(res){
+        console.log(res.data)
+        window.location.reload()
+    })
+}
+
+
+
+axios.get("http://ironrest.herokuapp.com/angelcomments").then(function(res){
+    console.log(res.data)
+})
