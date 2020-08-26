7acaf1e17e172fa34e72e037b8e49a6d8e7e1b07
second login <secondary@Matthews-MacBook-Pro-2.local>
2020-08-26 17:55:49 -0400

    recentupdate

--- index.js
+++ index.js
@@ -1,29 +1,61 @@
-let portfolioItems = document.querySelector('.portfolio-item-wrapper')
+// let portfolioItems = document.querySelector('.portfolio-item-wrapper')
+//
+//
+//
+//
+// function hover(){
+//
+// }
+//
+// portfolioItems.addEventListener('mouseover',hover)
+//
+//
+// document.querySelector("form").onsubmit = function (e){
+// e.preventDefault()
+// let comment = document.querySelector("#comments input").value
+//
+// console.log(comment)
+// axios.post("http://ironrest.herokuapp.com/angelcomments",{comment:comment})
+// .then(function(res){
+// console.log(res.data)
+// window.location.reload()
+// })
+// }
+//
+//
+//
+// axios.get("http://ironrest.herokuapp.com/angelcomments").then(function(res){
+// console.log(res.data)
+// })
+//
+//
+//
+//
 
+let firstToggle = document.querySelector("#submit-button");
 
+firstToggle.addEventListener("click", switchit);
 
-
-function hover(){
+function switchit() {
+  alert("Thanks for the feedback!")
 
 }
 
-portfolioItems.addEventListener('mouseover',hover)
+let name = document.querySelector("#name-text");
 
+let email = document.querySelector("#email");
 
-document.querySelector("form").onsubmit = function (e){
-    e.preventDefault()
-    let comment = document.querySelector("#comments input").value
-    
-    console.log(comment)
-    axios.post("http://ironrest.herokuapp.com/angelcomments",{comment:comment})
-    .then(function(res){
-        console.log(res.data)
-        window.location.reload()
-    })
-}
+// let submit= document.querySelector("#submit-button")
+
+let form = document.querySelector("#form")
+
+let message = document.querySelector("#message")
 
 
 
-axios.get("http://ironrest.herokuapp.com/angelcomments").then(function(res){
-    console.log(res.data)
-})
+// submit.addEventListener('click', cancelout)
+// 
+// function cancelout(){
+// 
+// 
+// }
